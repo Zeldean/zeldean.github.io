8ccde9210488659b794e9d814e65be7329bd2f92
Zeldean <deanvanzyl2004@gmail.com>
2025-07-28 15:29:19 +0200

    Force update site with cleaned files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-import { calculateAge } from "./age-calculation.mjs";
-
-document.addEventListener('DOMContentLoaded', () => {
-    const ageDisplay = document.getElementById('age_display');
-    const age = calculateAge('2004/11/22');
-    ageDisplay.textContent = age;
-
-})
